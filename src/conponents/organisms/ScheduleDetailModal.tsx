7d139a8f9cde41870f923c6ecd2830cd28a952ed
white-ras <rasras3366@gmail.com>
2024-05-15 14:08:41 +0900

    feat: scheduleDetailModalに削除ボタンを追加した

--- src/conponents/organisms/ScheduleDetailModal.tsx
+++ src/conponents/organisms/ScheduleDetailModal.tsx
@@ -1,6 +1,7 @@
 import Modal from 'react-modal'
 import { Schedule } from '@/types/calendar'
 import { format } from 'date-fns'
+import { PrimaryBtn } from '../atoms/PrimaryBtn'
 
 type PropsType = {
   selectedSchedule: Schedule | null
@@ -20,6 +21,22 @@ export const ScheduleDetailModal = ({
   selectedSchedule,
   closeModal,
 }: PropsType) => {
+  const deleteSchedule = () => {
+    const confirmationMessage: string = `${format(
+      selectedSchedule!.date,
+      'yyyy年M月d日'
+    )}:[${selectedSchedule!.title}]スケジュールを削除しますか？`
+    const confirmDelete = window.confirm(confirmationMessage)
+
+    if (confirmDelete) {
+      alert('データが削除されました')
+      closeModal()
+    } else {
+      alert('キャンセルされました')
+      closeModal()
+    }
+  }
+
   return (
     <Modal
       isOpen={!!selectedSchedule}
@@ -31,8 +48,15 @@ export const ScheduleDetailModal = ({
           <h3 className="text-center text-3xl text-lime-800 font-bold pb-5">
             {selectedSchedule.title}
           </h3>
+
           <p>{format(selectedSchedule.date, 'yyyy年M月d日')}</p>
+
           <p>{selectedSchedule.description}</p>
+          <div className="flex items-center text-white gap-4">
+            <PrimaryBtn size="sm" onClick={deleteSchedule}>
+              削除
+            </PrimaryBtn>
+          </div>
         </div>
       )}
     </Modal>
